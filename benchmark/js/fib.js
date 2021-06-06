21bd51259453bdfbfe7d8bf767609cc840a9b5ea
sparvu <sparvu@kronometrix.org>
2021-06-06 21:53:52 +0300

    added benchmark suite for Lua,JS,Perl

---
+++ benchmark/js/fib.js
@@ -0,0 +1,6 @@
+var fib = function(n) {
+  if (n <= 1) { return 1; }
+  return fib(n - 1) + fib(n - 2);
+};
+
+console.log(fib(46));
---
+++ benchmark/lua/num2.lua
@@ -0,0 +1,9 @@
+local x = 1
+
+for i=0,99999998 do
+	x = (i+i+2*i+1-0.379)/(x)
+end
+
+
+print(x)
+
